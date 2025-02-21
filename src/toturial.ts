07e97dcf7d078a9759783964e93dbc64472520a9
Adil Rahman <itsadil2022@gmail.com>
2025-02-21 20:08:21 +0600

    Started Typescript

---
+++ src/toturial.ts
@@ -0,0 +1,12 @@
+console.log("Typescript Learning From Youtube Tutorial");
+interface someValue {
+    name: string;
+    id: number;
+}
+
+let someObj: someValue = {
+    name: "random",
+    id: 123,
+}
+
+console.log(someObj);
