07bc8f4dae0b4c032480edc3d04a136fac15126a
Coded-Adil <itsadil2022@gmail.com>
2025-10-16 13:18:14 +0600

    interface types marge using extends

--- src/interfaceMarge.ts
+++ src/interfaceMarge.ts
@@ -12,6 +12,14 @@ interface Person{
     age: number;
 }
 
+interface Employee extends Person{
+    employeeId: number;
+}
+
+interface Manager extends Person, DogOwner{
+    managePeople(): void;
+}
+
 const person: Person = {
     name: "Alice",
     age: 30,
@@ -20,5 +28,33 @@ const person: Person = {
     }
 };
 
+const employee: Employee = {
+    name: "jane",
+    age: 28,
+    employeeId: 123,
+    getDetails(){
+        return `Name: ${this.name}, Employee ID: ${this.employeeId}, Age: ${this.age}`;
+    }
+}
+
+const manager: Manager = {
+    name: "Bob",
+    age: 40,
+    dogName: "Rex",
+    getDetails(){
+        return `Name: ${this.name}, Age: ${this.age}, Dog Name: ${this.dogName}`;
+    },
+    getDogDetails(){
+        return `Dog Name: ${this.dogName}`;
+    },
+    managePeople() {
+        console.log(`${this.name} is managing people.`);
+    },
+}
+
 
 console.log(person.getDetails());
+console.log(employee.getDetails());
+console.log(manager.getDetails());
+console.log(manager.getDogDetails());
+manager.managePeople();
